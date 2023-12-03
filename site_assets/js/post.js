edffbf01c85b6a54ade97b32d34b775a4f33b5d4
Fun117 <141471450+Fun117@users.noreply.github.com>
2023-12-03 14:04:09 +0900

    Delete site_assets directory

--- site_assets/js/post.js
+++
@@ -1,9 +0,0 @@
-function post_all_get_data(){
-    fetch('https://script.google.com/macros/s/AKfycbzILbtdUqEVA76vNP8TrXoeGZxbX_gSOUpMSwBzUcNzSkN1EIxYpZ2zIU04jR52JUDL/exec?mode=0', { mode: 'no-cors' });
-};
-function post_all_remove_data(){
-    fetch('https://script.google.com/macros/s/AKfycbzILbtdUqEVA76vNP8TrXoeGZxbX_gSOUpMSwBzUcNzSkN1EIxYpZ2zIU04jR52JUDL/exec?mode=1', { mode: 'no-cors' });
-};
-function post_add_data(){
-    fetch('https://script.google.com/macros/s/AKfycbzILbtdUqEVA76vNP8TrXoeGZxbX_gSOUpMSwBzUcNzSkN1EIxYpZ2zIU04jR52JUDL/exec?mode=accountAddData&email=&name=&password=', { mode: 'no-cors' });
-};
